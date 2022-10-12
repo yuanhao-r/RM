fbb17843bbdef098ca0a5c3a534c1d6a2f432f2b
zhaoyuanhao13419 <113402296+zhaoyuanhao13419@users.noreply.github.com>
2022-10-12 20:03:53 +0800

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 0;
+    cin >> a;
+    switch (a)
+    {
+        case 1:
+        cout << "wonderful" << endl;
+        break;
+        case 2:
+        cout << "perfect" << endl;
+        break;
+        default:
+        cout << "bad" << endl;
+    }
+}

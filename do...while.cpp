9f5376788388d9476493d1a7c0f0fe89e921eee3
zhaoyuanhao13419 <113402296+zhaoyuanhao13419@users.noreply.github.com>
2022-10-12 20:01:36 +0800

    Create do...while.cpp

---
+++ do...while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int e=1;
+    do
+    {
+        cout << e << endl;
+        e++;
+    }
+    while(e<10);
+    cout<<"水仙花数"<<endl;
+    int a = 100
+    
+
+}

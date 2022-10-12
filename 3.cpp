989c329befe1a47f4c0eef5c145990f3469f1839
zhaoyuanhao13419 <113402296+zhaoyuanhao13419@users.noreply.github.com>
2022-10-12 19:58:21 +0800

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    char number1 = ',';
+    cout << number1 << endl;
+    cout << "The size of char is " << sizeof(char) << endl;
+    cout << (int)number1 << endl;
+    cout << "hello\n";
+    cout << "\\\n\n" << endl <<endl <<endl;
+    cout << "haha\thello" << endl;
+    cout << "hahaha\thello\taaaaa" << endl;
+    cout << "hahahaha\thello" << endl;
+    cout << " hahahahah\thello" << endl;
+    char A = 'q';
+    cout << (int)A << endl;
+    char B[] ="asdfghjkl";
+    cout << B <<endl;
+    string C = "asdfasdfasdfasdf";
+    cout << C << endl;
+    bool E = true;
+    cout << E << "\n" << "\n" << endl;
+    cout << "The size od bool is " << sizeof(E) << endl; 
+    int F = 0;
+    cout << "Please put in the variable of F " << endl;
+    cin >> F ;
+    cout << F;
+
+    int G = 0;
+    cout << "请给G赋值" << endl;
+    cin >> G;            //从键盘中输入一些数据，放入变量中
+    cout << G << endl;
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+}

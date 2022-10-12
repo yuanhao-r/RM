5de1ef98a3419ff2b6fd05d2f1380ebc6da805b1
zhaoyuanhao13419 <113402296+zhaoyuanhao13419@users.noreply.github.com>
2022-10-12 20:01:07 +0800

    Create 水仙花数.cpp

---
+++ 水仙花数.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=100;
+    do
+    {
+        int a=0;
+        int b=0;
+        int c=0;
+        a=num/100;
+        b=num/10%10;
+        c=num%10;
+        if(a*a*a+b*b*b+c*c*c==num)
+        {
+            cout << num<< endl;
+        }
+        num++;
+    } while (num<1000);
+    
+}
